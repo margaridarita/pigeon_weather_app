948fe33f810c7ea2d5faba1329725c5c4bb6ff33
margaridarita <110779743+margaridarita@users.noreply.github.com>
2022-10-09 00:53:32 +0100

    Added description and icon
    Transformed details into a ul for it.

--- script.js
+++ script.js
@@ -74,11 +74,20 @@ let celTemp = null;
 // Get weather data from API
 
 function displayWeatherData(response) {
+  console.log(response.data);
   document.querySelector("#cidade").innerHTML = response.data.name;
   document.querySelector("#degrees").innerHTML = Math.round(
     response.data.main.temp
   );
   celTemp = Math.round(response.data.main.temp);
+  document
+    .querySelector("#icon")
+    .setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].description;
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
