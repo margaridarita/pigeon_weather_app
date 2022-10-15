0560990626856483081ada7258dcd276f29c92c6
margaridarita <110779743+margaridarita@users.noreply.github.com>
2022-10-15 14:35:39 +0100

    Added dynamic HTLM to forecast
    First column

--- script.js
+++ script.js
@@ -131,3 +131,32 @@ geoButton.addEventListener("click", userLocation);
 // Set initial value
 
 citySearch("Lisbon");
+
+// Build Forecast
+
+function displayForecast() {
+  
+  let forecastElement = document.querySelector("#forecast");    
+  
+  let forecastHTML= `<div class="row">`;
+  forecastHTML = forecastHTML + `
+    <div class="row">
+      <div class="col-2">
+        <div class="forecast-date">Thu</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="44"
+        />
+        <div class="forecast-temperatures">
+          <span class="forecast-temperature-max"> 18° </span>
+          <span class="forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+    </div>
+    `
+forecastHTML = forecastHTML + </div>;
+forecastElement.innerHTML = forecastHTML;
+}
+
+displayForecast();
